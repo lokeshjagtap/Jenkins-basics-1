3e1e6d56ecff85c4e7303f12ec20d12b1148f8f3
Your Name <you@example.com>
2025-09-05 16:41:07 +0530

    First commit

---
+++ src/com/example/Home.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is Home class");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BasicJenkinsProject1 {
+}
